21ff50d1cab14028741e81c2860007fde8bd270e
Lucien <lucien.lang@proton.me>
2024-12-17 12:33:00 +0100

    d17p1 first try

--- 2024/17/main.py
+++ 2024/17/main.py
@@ -16,11 +16,85 @@ def getInput():
     RegB = int(content[1].replace("\n", "").split(": ")[-1])
     RegC = int(content[2].replace("\n", "").split(": ")[-1])
 
-    return program, RegA, RegB, RegC
+    return program, np.array([RegA, RegB, RegC], dtype=int)
 
-def solve1(program, RegA, RegB, RegC):
-    
-    return 
+
+
+def combo(literal, Regs):
+    if literal in range(0,4):
+        return literal
+    if literal == 4:
+        return Regs[0]
+    if literal == 5:
+        return Regs[1]
+    if literal == 6:
+        return Regs[2]
+    raise Exception()
+
+
+def adv(Regs, literal, index = 0):
+    Regs[0] = Regs[index] // (2 ** combo(literal, Regs))
+    return
+
+def bxl(Regs, literal):
+    Regs[1] ^ literal
+    return
+
+def bst(Regs, literal):
+    # do chammer potentiel bitwise and shenanegans mache
+    Regs[1] = combo(literal, Regs) % 8
+    return
+
+
+
+
+
+def solve1(program, Regs):
+    i = 0
+    output = ""
+    while i < len(program)-1:
+        print(i)
+        opcode = program[i]
+        literal = program[i+1]
+        
+        if opcode == 0:
+            adv(Regs, literal)
+            i+=2
+            continue
+        if opcode == 1:
+            bxl(Regs, literal)
+            i+=2
+            continue
+        if opcode == 2:
+            bst(Regs, literal)
+            i+=2
+            continue
+        if opcode == 3:
+            if not Regs[0]:
+                i+=2
+                continue
+            # if i == len(program)-2:
+            #     break
+            i = literal    
+            continue
+        if opcode == 4:
+            Regs[1] ^= Regs[2] 
+            i+=2
+            continue
+        if opcode == 5:
+            output += str(combo(literal, Regs)%8) + ","
+            i+=2
+            continue
+        if opcode == 6:
+            adv(Regs, literal, 1)
+            i+=2
+            continue
+        if opcode == 7:
+            adv(Regs, literal, 2)
+            i+=2
+            continue
+
+    return output[:len(output)-1]
 
 
 
@@ -33,6 +107,6 @@ def solve2(input):
 
 
 if __name__ == "__main__":
-    program, RegA, RegB, RegC = getInput()
-    print(solve1(program, RegA, RegB, RegC))
+    program, Regs = getInput()
+    print(solve1(program, Regs))
     print(solve2(input))
